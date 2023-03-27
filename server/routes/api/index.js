3405d5abec27ce92af4c6336746ac0cfd3571fca
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-27 15:27:03 +1100

    Added historical market routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRouter = require("./api/userRouter");
+const marketRouter = require("./api/marketRouter");
 
 router.use("/user", userRouter);
+router.use("/market", marketRouter);
 
 module.exports = router;
--- server/routes/api/marketRouter.js
+++ server/routes/api/marketRouter.js
@@ -17,3 +17,36 @@ router.get("/market", async (req, res) => {
     res.status(500).json({ message: "An error occured" });
   }
 });
+
+router.get("/historical", async (req, res) => {
+  const { id, days } = req.query;
+  try {
+    const response = await fetch(
+      `${URL}coins/${id}/market_chart?vs_currency=aud&days=${days}`
+    );
+    const data = await response.json();
+    if (!data.prices) {
+      return res.status(400).json({ message: "Error fetching coin data" });
+    }
+    const sortedData = formatData(data);
+    res.json(sortedData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "An error occured" });
+  }
+});
+
+function formatData(historicalData) {
+  let obj = {};
+  for (const key in historicalData) {
+    for (let i = 0; i < historicalData[key].length; i++) {
+      const date = dayjs(historicalData[key][i][0]);
+      const price = historicalData[key][i][1];
+      if (!obj[key]) {
+        obj[key] = [];
+      }
+      obj[key].push({ date, price });
+    }
+  }
+  return obj;
+}

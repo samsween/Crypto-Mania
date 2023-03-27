ec8d8c23972bae69a32df445724f8d2d4c3c270d
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-27 19:09:45 +1100

    Removed unused imorts

--- server/routes/api/userRouter.js
+++ server/routes/api/userRouter.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const User = require("../../models/User");
 const { login, register } = require("../../controller/userController");
 
 router.post("/register", login);

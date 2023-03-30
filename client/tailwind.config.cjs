ca4f6dfdd796cfa9fda02991a439896927fee022
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-30 13:34:28 +1100

    Changed config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#131519",
+          200: "#25282C",
+          300: "#191A1E",
+        },
+      },
+    },
   },
   plugins: [],
 };

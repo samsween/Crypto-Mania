2b47b4db6cb63aba058ce7269cea4c9e1e1b793e
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-30 17:54:02 +1100

    Fixed styling

--- client/src/components/mainlayout/index.jsx
+++ client/src/components/mainlayout/index.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../../context/authContext";
-import { Menu } from "tabler-icons-react";
+import {  Menu2 } from "tabler-icons-react";
 import { AnimatePresence } from "framer-motion";
 import { SideMenu } from "./SideMenu";
 import { useState } from "react";
@@ -17,11 +17,11 @@ const MainLayout = () => {
       </aside>
       <div className="flex-1 w-full min-h-screen bg-primary-300">
         <header className="w-full h-20 text-gray-200 items-center bg-primary-100 flex justify-between lg:justify-end">
-          <div className="lg:hidden flex w-full h-full px-4">
-            <Menu  onClick={()=>setMobileMenu(true)}/>
+          <div className="lg:hidden flex px-4">
+            <Menu2  onClick={()=>setMobileMenu(true)}/>
             <MobileMenu open={mobileMenu} setOpen={setMobileMenu} />
           </div>
-          <div className="md:flex md:pr-8 md:gap-8  md:flex-row flex-col gap-2 px-4 ">
+          <div className="md:flex md:pr-8 md:gap-8 items-center md:flex-row flex-col gap-2 px-4 ">
             <h1>
               Current cash{" "}
               <span className="text-orange-500">

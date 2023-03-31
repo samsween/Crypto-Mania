63df9ba239b2e4c584372f3519a7c88e55c203d3
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-31 13:36:36 +1100

    Cleanup

--- client/src/components/mainlayout/index.jsx
+++ client/src/components/mainlayout/index.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../../context/authContext";
-import {  Menu2 } from "tabler-icons-react";
+import { Menu2 } from "tabler-icons-react";
 import { AnimatePresence } from "framer-motion";
 import { SideMenu } from "./SideMenu";
 import { useState } from "react";
@@ -8,7 +8,6 @@ import { MobileMenu } from "./mobileMenu";
 const MainLayout = () => {
   const { user } = useUser();
   const [mobileMenu, setMobileMenu] = useState(false);
-  console.log(user);
   if (!user) return <Navigate to="/login" />;
   return (
     <div className="flex">
@@ -18,7 +17,7 @@ const MainLayout = () => {
       <div className="flex-1 w-full min-h-screen bg-primary-300">
         <header className="w-full h-20 text-gray-200 items-center bg-primary-100 flex justify-between lg:justify-end">
           <div className="lg:hidden flex px-4">
-            <Menu2  onClick={()=>setMobileMenu(true)}/>
+            <Menu2 onClick={() => setMobileMenu(true)} />
             <MobileMenu open={mobileMenu} setOpen={setMobileMenu} />
           </div>
           <div className="md:flex md:pr-8 md:gap-8 items-center md:flex-row flex-col gap-2 px-4 ">

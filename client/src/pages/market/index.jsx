5d0dc5d27aa7948de94ade1b638ead6f3e4d2bae
Sam Sweeney <samsweeneyy@gmail.com>
2023-04-01 22:56:50 +1100

    removed market data

--- client/src/pages/market/index.jsx
+++ client/src/pages/market/index.jsx
@@ -1,21 +1,14 @@
 import { useQuery } from "react-query";
 
 const Market = () => {
-  const { error, isLoading, data } = useQuery(
-    "market",
-    () => {
-      return fetch("http://localhost:3000/api/market").then((res) =>
-        res.json()
-      );
-    },
-
-  );
+  const { error, isLoading, data } = useQuery("market", () => {
+    return fetch("http://localhost:3000/api/market").then((res) => res.json());
+  });
   return (
     <div>
       <h1>Market</h1>
       {error && <div>Something went wrong</div>}
       {isLoading && <div>Loading...</div>}
-      {data && <div>{JSON.stringify(data)}</div>}
     </div>
   );
 };

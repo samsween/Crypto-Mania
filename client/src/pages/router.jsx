a778762984d50a0143d5fa7c672f914847dfccf5
Sam Sweeney <samsweeneyy@gmail.com>
2023-04-05 13:53:54 +1000

    Added home and coin routes

--- client/src/pages/router.jsx
+++ client/src/pages/router.jsx
@@ -1,19 +1,24 @@
-const {
+import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-} = require("react-router-dom");
-import Login from "./pages/login";
-import MainLayout from "./components/mainlayout";
-import Register from "./pages/register";
-import Market from "./pages/market";
+  Navigate,
+} from "react-router-dom";
+import Login from "./login";
+import MainLayout from "../components/mainlayout";
+import Register from "./register";
+import Market from "./market";
+import Coin from "./coin";
+import Home from "./home";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Market />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/market:id" element={<Coin />} />
       </Route>
     </Route>
   )

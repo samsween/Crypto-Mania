7c7bb7dce93e3f99e605c231625daf3e91bf87b1
Sam Sweeney <samsweeneyy@gmail.com>
2023-04-05 12:41:26 +1000

    Added proxy to localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });

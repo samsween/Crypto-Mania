87d329994f74123227a41e7f581e54270b509078
Sam Sweeney <samsweeneyy@gmail.com>
2023-04-06 14:39:46 +1000

    Animation

--- client/src/pages/coin/components/PriceTable.jsx
+++ client/src/pages/coin/components/PriceTable.jsx
@@ -1,15 +1,19 @@
+import { motion } from "framer-motion";
 export const PriceTable = ({ data }) => {
   return (
     <table>
       <tbody className="w-full">
-        {data.map((item) => (
-          <tr
+        {data.map((item, index) => (
+          <motion.tr
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
             className="border-b border-orange-500 border-opacity-40 py-8 whitespace-nowrap"
             key={item.name}
           >
             <td>{item.name}</td>
             <td className="px-8">{item.value}</td>
-          </tr>
+          </motion.tr>
         ))}
       </tbody>
     </table>

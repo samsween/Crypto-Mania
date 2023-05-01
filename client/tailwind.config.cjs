6c4e6eb5524f75e9e582128a7232f5229b94b92c
Sam Sweeney <samsweeneyy@gmail.com>
2023-05-01 18:39:50 +1000

    Added color animation on price change

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,12 +3,26 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "change-red": {
+          "0%, 100%": { color: "" },
+          "50%": { color: "#25282C" },
+        },
+        "change-green": {
+          "0%, 100%": { color: "#131519" },
+          "50%": { color: "#191A1E" },
+        },
+      },
       colors: {
         primary: {
           100: "#131519",
           200: "#25282C",
           300: "#191A1E",
         },
+      },
+      animation: {
+        "animate-change-red": "change-red 1s ease-in-out",
+        "animate-change-green": "change-green 1s ease-in-out",
       },
     },
   },

3820b69f921236d3077b3e46988ac5a64aa154fc
Sam Sweeney <samsweeneyy@gmail.com>
2023-05-04 09:44:39 +1000

    Refactor

--- client/src/pages/wallet/components/SellForm.jsx
+++ client/src/pages/wallet/components/SellForm.jsx
@@ -5,9 +5,10 @@ import { Loader } from "../../../components/Loader";
 export const SellForm = ({ id, symbol, total }) => {
   const [price, setPrice] = useState(null);
   const [totalToSell, setTotalToSell] = useState("0");
+  const [val, setVal] = useState(0);
   useEffect(() => {
     const onPrice = (value) => {
-      setPrice(value.price);
+      setPrice(parseFloat(value.price));
     };
     socket.emit("get-price", id);
     socket.on("price", onPrice);
@@ -18,22 +19,25 @@ export const SellForm = ({ id, symbol, total }) => {
   }, []);
 
   const onTotalToSellChange = (e) => {
-    const regex = /^[0-9\b]+$/;
-    if ( regex.test(e.target.value)) {
-      setVal(e.target.value);
-    }
-    if (isNaN(e.target.value)) return setTotalToSell(0);
+    var countDecimals = function (value) {
+      if (Math.floor(value) === value) return 0;
+      return value.toString().split(".")[1].length || 0;
+    };
+    if (isNaN(e.target.value) && e.target.value !== "")
+      return setTotalToSell(0);
+    if (countDecimals(parseFloat(e.target.value)) > 8) return;
     if (e.target.value > total) {
-      setTotalToSell(total);
+      setTotalToSell(total.toFixed(8));
     } else {
       setTotalToSell(e.target.value);
     }
     if (e.target.value < 0) {
       setTotalToSell(0);
     }
+    fetch("/api/")
   };
   return (
-    <div className="w-full h-full bg-primary-100 p-10 text-gray-300">
+    <div className="w-full h-full bg-primary-100 p-10 flex flex-col justify-between  text-gray-300">
       <form>
         <h1 className="text-2xl font-bold text-center flex w-full items-center justify-center gap-8">
           Current Price:{" "}
@@ -46,7 +50,7 @@ export const SellForm = ({ id, symbol, total }) => {
           )}
         </h1>
         <h2 className="text-xl font-bold text-center flex w-full items-center justify-center gap-8">
-          Your wallet: {total}
+          Your wallet: {parseFloat(total).toFixed(8)}
         </h2>
         <div>
           <label className="text-xl" htmlFor="total">
@@ -57,12 +61,23 @@ export const SellForm = ({ id, symbol, total }) => {
             onChange={onTotalToSellChange}
             name="total"
             id="total"
-            className="w-full h-10  px-2 bg-transparent border-b border-orange-500"
+            className="w-full h-10 px-2 bg-transparent border-b border-orange-500"
             type="number"
           />
         </div>
       </form>
-      <div className="flex justify-center mt-10">
+      <h1 className="text-2xl font-bold text-center flex w-full items-center justify-center gap-8">
+        Total Sell Value:
+        {price ? (
+          <span>${parseFloat(price * totalToSell).toFixed(8)}</span>
+        ) : (
+          <span>
+            <Loader />
+          </span>
+        )}
+      </h1>
+
+      <div className="flex justify-center items-center">
         <button className="bg-orange-500 w-[90%] text-primary-100 py-4 rounded-md">
           Sell {symbol.toUpperCase()}
         </button>
